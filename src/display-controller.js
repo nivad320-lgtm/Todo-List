3d6a1a199a7bb3b8e7a2848458acd0533ca115df
Davin <286156958+nivad320-lgtm@users.noreply.github.com>
2026-07-02 16:12:51 +0900

    Add basic form to add Todo

--- src/display-controller.js
+++ src/display-controller.js
@@ -1,10 +1,11 @@
 const DisplayController = {
-    buildFormRow(labelFor, type, formID, labelName) {
+    buildFormRow(type, labelFor, formID, labelName) {
         const formRow = document.createElement('div');
+        formRow.setAttribute('class', 'formRow');
         const label = document.createElement('label');
         const input = document.createElement('input');
         label.setAttribute('for', labelFor);
-        label.textContent = labelName;
+        label.textContent = `${labelName}: `;
         
         input.setAttribute('type', type);
         input.setAttribute('name', formID);
@@ -18,14 +19,47 @@ const DisplayController = {
         return formRow;
     },
 
+    buildFormRowSelect(selectName, labelFor, formID, values, labelName) {
+        const formRow = document.createElement('div');
+        formRow.setAttribute('class', 'formRow');
+
+        const label = document.createElement('label');
+        label.setAttribute('for', labelFor);
+        label.textContent = `${labelName}: `;
+
+        const select = document.createElement('select');
+        select.setAttribute('name', selectName);
+        select.setAttribute('id', formID);
+
+        for (let value of values) {
+            const option = document.createElement('option');
+            option.setAttribute('value', value);
+            option.textContent = value;
+            select.appendChild(option);
+        }
+
+        formRow.append(
+            label,
+            select,
+        )
+        return formRow
+        
+    },
+
     loadHome(divID) {
         const containerDiv = document.querySelector(divID)
         const newForm = document.createElement('form');
 
+        const btn = document.createElement('button');
+        btn.textContent = 'Add';
 
         containerDiv.append(
             newForm,
-            this.buildFormRow('name', 'text', 'name', 'Title')
+            this.buildFormRow('text', 'name', 'name', 'Title'),
+            this.buildFormRow('text', 'description', 'description', 'Description'),
+            this.buildFormRow('date', 'dueDate', 'dueDate', 'Due Date'),
+            this.buildFormRowSelect('priority', 'priority-select', 'priority-select', ['Low', 'Medium', 'High'], 'Priority'),
+            btn,
         )
 
 
