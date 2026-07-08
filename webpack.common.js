8044ddc851bcc9683029d35b268968f1ea8bf8b2
Davin <286156958+nivad320-lgtm@users.noreply.github.com>
2026-07-08 22:13:52 +0900

    Add publicpath on webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -8,6 +8,7 @@ export default {
   output: {
     filename: "main.js",
     path: path.resolve(import.meta.dirname, "dist"),
+    publicPath: "",
     clean: true,
   },
   devtool: "eval-source-map",
